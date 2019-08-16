16465ad5607ef829042198081f057b4ae544957f
kcarullo <35302366+kcarullo@users.noreply.github.com>
2019-08-16 14:37:26 -0400

    Add files via upload

--- Minimize Churn/model.py
+++ Minimize Churn/model.py
@@ -8,7 +8,7 @@ import random
 dataset = pd.read_csv('new_churn_data.csv')
 
 ### Data Prep
-user_indentifier = dataset['user']
+user_identifier = dataset['user']
 dataset = dataset.drop(columns = ['user'])
 
 # One-Hot Encoding 
@@ -21,9 +21,9 @@ dataset = dataset.drop(columns = ['housing_na', 'zodiac_sign_na', 'payment_type_
 #splitting the dataset into training and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(dataset.drop(columns='churn'),
-                                                    dataset['churn'],
-                                                    test_size = 0.2,
-                                                    random_state = 0)
+                                dataset['churn'],
+                                test_size = 0.2,
+                                random_state = 0)
 
 # Balancing the Training set
 y_train.value_counts()
@@ -49,15 +49,14 @@ y_train = y_train.loc[new_indexes]
 
 
 # Feature Scaling 
-
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train2 = pd.DataFrame(sc_X.fit_transform(X_train))
-X_test2 = pd.DataFrame(sc_X.fit_transform(X_test))
+X_test2 = pd.DataFrame(sc_X.transform(X_test))
 X_train2.columns = X_train.columns.values
 X_test2.columns = X_test.columns.values
-X_train2.index = X_train.columns.values
-X_test2.index = X_test.columns.values
+X_train2.index = X_train.index.values
+X_test2.index = X_test.index.values
 X_train = X_train2
 X_test = X_test2
 
@@ -97,12 +96,55 @@ accuracies = cross_val_score(classifier,
 
 # Analyzing Coefficients 
 pd.concat([pd.DataFrame(X_train.columns, columns = ['features']),
+           pd.DataFrame(np.transpose(classifier.coef_), columns =   ['coef'])], axis = 1)
+
+# FEature Selection
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+
+# Model to Test
+classifier = LogisticRegression()
+rfe = RFE(classifier, 20)
+rfe = rfe.fit(X_train, y_train)
+
+# summarize the selection   
+print(rfe.support_)
+
+X_train.columns[rfe.support_]
+rfe.ranking_
+
+## Use X_train.columns[rfe.support_] 
+# Fitting Model to the Training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train[X_train.columns[rfe.support_]], y_train)
+
+
+# Predicting Test Set
+y_pred = classifier.predict(X_test[X_test.columns[rfe.support_]])
+
+# Evaluating Results 
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+
+cm = confusion_matrix(y_test, y_pred)
+accuracy_score(y_test, y_pred)
+precision_score(y_test, y_pred)
+f1_score(y_test, y_pred)
+recall_score(y_test, y_pred)
+
+df_cm = pd.DataFrame(cm, index = (0,1), columns = (0,1))
+plt.figure(figsize = (10,7))
+sns.set(font_scale=1.4)
+sns.heatmap(df_cm, annot=True, fmt='g')
+print('Test Data Accuaracy: %0.4f' % accuracy_score(y_test, y_pred))
+
+# Analyzing coefficients 
+pd.concat([pd.DataFrame(X_train.columns[rfe.support_], columns = ['features']),
            pd.DataFrame(np.transpose(classifier.coef_), columns = ['coef'])],
-           axis = 1)
+    axis = 1)
 
-
-
-
-
-
-
+## formating final results 
+final_results = pd.concat([y_test, user_identifier], axis = 1).dropna()
+final_results['predicted_churn'] = y_pred
+final_results = final_results[['user', 'churn', 'predicted_churn']].reset_index(drop=True)
+final_results
